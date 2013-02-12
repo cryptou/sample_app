efc60b64d0bed91e9db57e98cc1d0ce88d3870d7
cryptou <cryptou@gmail.com>
2013-02-12 17:08:16 +0800

    Finish updating users

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,29 @@
 include ApplicationHelper
 
+def sign_in(user)
+  visit signin_path
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  # Sign in when not using Capybara.
+  cookies[:remember_token] = user.remember_token
+end
+
+RSpec::Matchers.define :have_success_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-success')
+  end
+end
+
 RSpec::Matchers.define :have_error_message do |message|
   match do |page|
     page.should have_selector('div.alert.alert-error')
+  end
+end
+
+RSpec::Matchers.define :have_notice_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-notice')
   end
 end
 
@@ -15,11 +36,5 @@ end
 RSpec::Matchers.define :have_top_level_header do |header_text|
   match do |page|
     page.should have_selector('h1', text: header_text)
-  end
-end
-
-RSpec::Matchers.define :have_success_message do |message|
-  match do |page|
-    page.should have_selector('div.alert.alert-success')
   end
 end
